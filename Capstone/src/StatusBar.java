b9e34340d8e1c6c17a374c21f1d65ec9c00d0fc1
hayesorhaze <matthew.hayes.09@cnu.edu>
2013-04-17 19:49:20 -0300

    Update StatusBar.java

--- Capstone/src/StatusBar.java
+++ Capstone/src/StatusBar.java
@@ -11,41 +11,33 @@ import java.sql.ResultSet;
 
 public class StatusBar extends JLabel
 {	
-	boolean isConnected = false;
-	private String urlR = "jdbc:mysql://lampa.vf.cnu.edu:3306/";
-	private String dbNameR = "feastdb";
-	private String driverR = "com.mysql.jdbc.Driver";
-	private String userNameR = "root";
-	private String passwordR = "lampa";
-	private Connection connR = null;
+	boolean isUpdating = false;
 	private Connection connL = null;
 	
-	public StatusBar(Connection connR, Connection connL)
+	public StatusBar(Connection connL, boolean update)
 	{
 		super();
 		super.setPreferredSize(new Dimension(100, 16));
-		isConnected = canConnect();
-		Timer t = new Timer();
-		t.scheduleAtFixedRate(new TimerTask()
-		{
-			public void run()
-			{
-				if(canConnect())
-				{
-					setMessage("Online");
-				}
-				else
-				{
-					setMessage("Local");
-				}
-			}
-		}, 0, 10000L);
+		isUpdating = update;
 	}
 	
 	public void setMessage(String message)
 	{
 		//if()
 		setText(" " + message);
+	}
+	
+	public void refresh(boolean update)
+	{
+	    isUpdating = update;
+	    if(isUpdating)
+        {
+            setMessage("Updating...");
+        }
+        else
+        {
+            setMessage("Local");
+        }
 	}
 	
 	public void playMessage(String message)
@@ -56,9 +48,9 @@ public class StatusBar extends JLabel
 		{
 			public void run()
 			{
-				if(canConnect())
+				if(isUpdating)
 				{
-					setMessage("Online");
+					setMessage("Updating");
 				}
 				else
 				{
@@ -66,20 +58,5 @@ public class StatusBar extends JLabel
 				}
 			}
 		}, 5000);
-	}
-	public boolean canConnect()
-	{
-		isConnected = false;
-		try
-		{
-			Class.forName(driverR).newInstance();
-			connR = DriverManager.getConnection(urlR + dbNameR, userNameR, passwordR);
-			isConnected = true;
-		}
-		catch(Exception e)
-		{
-			// e.printStackTrace();
-		}
-		return isConnected;
 	}
 }
