813c66be9890e3e9ea0bb3cad225e0f80dc31e80
hayesorhaze <matthew.hayes.09@cnu.edu>
2013-04-17 19:47:07 -0300

    Update FBMonthlyDistribution.java

--- Capstone/src/FBMonthlyDistribution.java
+++ Capstone/src/FBMonthlyDistribution.java
@@ -2,209 +2,183 @@ import java.sql.*;
 
 public class FBMonthlyDistribution
 {
-	private int distribution_ID;
-	private Connection connL = null;
-	private ResultSet rsL = null;
-	private Statement stmtL = null;
+    private int customer_ID;
+    private String acct_num;
+    private int agencyRep_ID;
+    private String date;
 
-	public FBMonthlyDistribution(Connection aConnR, Connection aConnL,
-			boolean aIsConnected, int aCustomerID, int aAgencyID,
-			int aAgencyRepID, String date) throws SQLException
-	{
-		connL = aConnL;
-		PreparedStatement prepL = connL
-				.prepareStatement("insert into fb_monthlyDist values (?, ?, ?, ?, ?);");
-		String sql = "SELECT * FROM fb_monthlyDist WHERE Distribution_ID = (SELECT MAX(Distribution_ID) FROM fb_monthlyDist);";
-		stmtL = connL.createStatement();
-		rsL = stmtL.executeQuery(sql);
-		if(rsL.next())
-		{
-			distribution_ID = rsL.getInt("Agency_ID") + 1;
-		}
-		else
-		{
-			distribution_ID = 1;
-		}
-		prepL.setInt(1, distribution_ID);
-		prepL.setInt(2, aCustomerID);
-		prepL.setInt(3, aAgencyID);
-		prepL.setInt(4, aAgencyRepID);
-		prepL.setString(5, date);
-		prepL.addBatch();
-		connL.setAutoCommit(false);
-		prepL.executeBatch();
-		connL.setAutoCommit(true);
-	}
+    private Connection connL = null;
+    private ResultSet rsL = null;
+    private Statement stmtL = null;
+    queryQue que;
 
-	public FBMonthlyDistribution(Connection aConnL, boolean aIsConnected,
-			int aDistributionID) throws SQLException
-	{
-		distribution_ID = aDistributionID;
-		connL = aConnL;
-	}
+    public FBMonthlyDistribution( Connection aConnL,
+            boolean aIsConnected, int aCustomerID, String aAcctNum,
+            int aAgencyRepID, String adate, queryQue q) throws SQLException
+    {
+        customer_ID = aCustomerID;
+        acct_num = aAcctNum;
+        agencyRep_ID = aAgencyRepID;
+        date = adate;
+        connL = aConnL;
+        stmtL = connL.createStatement();
 
-	/*
-	 * public String getCreateMonthlyDistQuery() { String insertCustQuery =
-	 * "INSERT INTO fb_monthlyDist(Distribution_ID, Customer_ID,Agency_ID,AgencyRep_ID,theDate) VALUES ("
-	 * ; String temp = insertCustQuery + "'" + agency_ID + "','" + getAcctNum()
-	 * + "','" + getAgencyName() + "');"; return temp; }
-	 */
+        que = q;
+        PreparedStatement prepL = connL
+                .prepareStatement("insert into jos_fb_monthlydist values (?, ?, ?, ?);");
+     
+        prepL.setInt(1, customer_ID);
+        prepL.setString(2, acct_num);
+        prepL.setInt(3, agencyRep_ID);
+        prepL.setString(4, date);
+        System.out.println(prepL.toString());
+        prepL.addBatch();
+        connL.setAutoCommit(false);
+        prepL.executeBatch();
+        connL.setAutoCommit(true);
+    }
 
-	public int getCustomerID()
-	{
-		int customerID = -1;
-		try
-		{
-			String sql = "SELECT * FROM fb_monthlyDist WHERE Distribution_ID = "
-					+ distribution_ID;
-			stmtL = connL.createStatement();
-			rsL = stmtL.executeQuery(sql);
-			rsL.next();
-			customerID = rsL.getInt("Customer_ID");
-			stmtL.close();
-			rsL.close();
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return customerID;
-	}
+    public FBMonthlyDistribution(Connection aConnL, boolean aIsConnected,
+            int aCustomerID, String aAcctNum, String adate) throws SQLException
+    {
+        customer_ID = aCustomerID;
+        acct_num = aAcctNum;
+        date = adate;
 
-	public String setCustomerID(int aCustomerID)
-	{
-		try
-		{
-			connL.setAutoCommit(true);
-			String sql = "UPDATE fb_monthlyDist SET Customer_ID= '"
-					+ aCustomerID + "' WHERE Distribution_ID="
-					+ distribution_ID;
-			PreparedStatement st = connL.prepareStatement(sql);
-			st.execute();
-			return sql;
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return "";
-	}
+        connL = aConnL;
+    }
 
-	public int getAgencyID()
-	{
-		int agencyID = -1;
-		try
-		{
-			String sql = "SELECT * FROM fb_monthlyDist WHERE Distribution_ID = "
-					+ distribution_ID;
-			stmtL = connL.createStatement();
-			rsL = stmtL.executeQuery(sql);
-			rsL.next();
-			agencyID = rsL.getInt("Agency_ID");
-			stmtL.close();
-			rsL.close();
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return agencyID;
-	}
+    /*
+     * public String getCreateMonthlyDistQuery() { String insertCustQuery =
+     * "INSERT INTO fb_monthlyDist(Distribution_ID, Customer_ID,Agency_ID,AgencyRep_ID,theDate) VALUES ("
+     * ; String temp = insertCustQuery + "'" + agency_ID + "','" + getAcctNum()
+     * + "','" + getAgencyName() + "');"; return temp; }
+     */
 
-	public String setAgencyID(int aAgencyID)
-	{
-		try
-		{
-			connL.setAutoCommit(true);
-			String sql = "UPDATE fb_monthlyDist SET Agency_ID= '" + aAgencyID
-					+ "' WHERE Distribution_ID=" + distribution_ID;
-			PreparedStatement st = connL.prepareStatement(sql);
-			st.execute();
-			return sql;
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return "";
-	}
+    public int getCustomerID()
+    {
+        return customer_ID;
 
-	public int getAgencyRepID()
-	{
-		int agencyRepID = -1;
-		try
-		{
-			String sql = "SELECT * FROM fb_monthlyDist WHERE Distribution_ID = "
-					+ distribution_ID;
-			stmtL = connL.createStatement();
-			rsL = stmtL.executeQuery(sql);
-			rsL.next();
-			agencyRepID = rsL.getInt("AgencyRep_ID");
-			stmtL.close();
-			rsL.close();
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return agencyRepID;
-	}
+    }
 
-	public String setAgencyRepID(int aAgencyRepID)
-	{
-		try
-		{
-			connL.setAutoCommit(true);
-			String sql = "UPDATE fb_monthlyDist SET AgencyRep_ID= '"
-					+ aAgencyRepID + "' WHERE Distribution_ID="
-					+ distribution_ID;
-			PreparedStatement st = connL.prepareStatement(sql);
-			st.execute();
-			return sql;
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return "";
-	}
+    public String setCustomerID(int aCustomerID)
+    {
+        try
+        {
+            connL.setAutoCommit(true);
+            String sql = "UPDATE jos_fb_monthlydist SET Customer_ID= '"
+                    + aCustomerID + "' WHERE Customer_ID = "
+                    + customer_ID + " AND Acct_Num = '" + acct_num + "' AND theDate = '" + date + "';";
 
-	public Date getDate()
-	{
-		Date date = null;
-		try
-		{
-			String sql = "SELECT * FROM fb_monthlyDist WHERE Distribution_ID = "
-					+ distribution_ID;
-			stmtL = connL.createStatement();
-			rsL = stmtL.executeQuery(sql);
-			rsL.next();
-			date = rsL.getDate("theDate");
-			stmtL.close();
-			rsL.close();
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return date;
-	}
+            PreparedStatement st = connL.prepareStatement(sql);
+            st.execute();
+            customer_ID = aCustomerID;
 
-	public String setDate(Date date)
-	{
-		try
-		{
-			connL.setAutoCommit(true);
-			String sql = "UPDATE fb_monthlyDist SET theDate= '" + date
-					+ "' WHERE Distribution_ID=" + distribution_ID;
-			PreparedStatement st = connL.prepareStatement(sql);
-			st.execute();
-			return sql;
-		}
-		catch(SQLException e)
-		{
-			e.printStackTrace();
-		}
-		return "";
-	}
+            return sql;
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getAcctNum()
+    {
+       
+        return acct_num;
+    }
+
+    public String setAcctNum(String aAcctnum)
+    {
+        try
+        {
+            connL.setAutoCommit(true);
+            String sql = "UPDATE jos_fb_monthlydist SET Acct_Num = '" + aAcctnum
+                    + "' WHERE Customer_ID =" + customer_ID + " AND theDate = '" + date + "' AND Acct_Num = '" + acct_num + "';";
+
+            PreparedStatement st = connL.prepareStatement(sql);
+            st.execute();
+            acct_num = aAcctnum;
+
+            return sql;
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public int getAgencyRepID()
+    {
+        int agencyRepID = -1;
+        try
+        {
+            String sql = "SELECT * FROM jos_fb_monthlydist WHERE Customer_ID = "
+                    + customer_ID + " AND Acct_Num = '" + acct_num + "' AND theDate = '" + date + "';";
+
+            stmtL = connL.createStatement();
+            rsL = stmtL.executeQuery(sql);
+            rsL.next();
+            agencyRepID = rsL.getInt("AgencyRep_ID");
+            stmtL.close();
+            rsL.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return agencyRepID;
+    }
+
+    public String setAgencyRepID(int aAgencyRepID)
+    {
+        try
+        {
+            connL.setAutoCommit(true);
+            String sql = "UPDATE jos_fb_monthlydist SET AgencyRep_ID= '"
+                    + aAgencyRepID + "' WHERE Customer_ID = "
+                    + customer_ID + " AND Acct_Num = '" + acct_num + "' AND theDate = '" + date + "';";
+
+            PreparedStatement st = connL.prepareStatement(sql);
+            st.execute();
+            return sql;
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getDate()
+    {
+        return date;
+    }
+
+    public String setDate(String adate)
+    {
+        try
+        {
+            connL.setAutoCommit(true);
+            String sql = "UPDATE jos_fb_monthlydist SET theDate= '" + adate
+                    + "' WHERE Customer_ID=" + customer_ID + " AND Acct_Num = '" + acct_num + "' AND theDate = '" + date + "';";
+
+            PreparedStatement st = connL.prepareStatement(sql);
+            st.execute();
+            return sql;
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return "";
+    }
+    public String createQuery(){
+        return "INSERT into jos_fb_monthlydist(Customer_ID, Acct_Num, AgencyRep_ID, theDate) VALUES (" + getCustomerID() + ", '" + getAcctNum()
+        + "', " + getAgencyRepID() + ", '" + getDate() + "');";
+    }
 }
+
+   
